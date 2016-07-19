060a5203c4bfb102fb283b7aa023b55c8eae2562
hzleishaobo <hzleishaobo@corp.netease.com>
2016-07-19 16:06:38 +0800

    conflict

--- cafe-wedding-common/src/main/java/com/cafe/wedding/common/App.java
+++ cafe-wedding-common/src/main/java/com/cafe/wedding/common/App.java
@@ -3,7 +3,10 @@ package com.cafe.wedding.common;
 public class App {
 
 	public static void main(String[] args) {
-
+		double a1=3.4;
+		double a2=0.2;
+		double b=a1-a2;
+		System.out.println(b);
 	}
 
 }
