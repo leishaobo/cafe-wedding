8e6d98353796a9872ade8acd4a86152db57a00e8
hzleishaobo <hzleishaobo@corp.netease.com>
2016-07-20 13:47:58 +0800

    添加demo子模块

---
+++ cafe-wedding-demo/src/test/java/com/cafe/wedding/seachable/SeachableTest.java
@@ -0,0 +1,14 @@
+package com.cafe.wedding.seachable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeachableTest {
+
+	@Test
+	public void test() {
+		System.out.println("a");
+	}
+
+}
